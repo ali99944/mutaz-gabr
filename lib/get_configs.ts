d49fd580df53eb6ca3c5573232182ae61eed833a
ali99944 <alitarek99944@gmail.com>
2025-01-09 02:45:19 +0200

    9-1-2024 update

--- lib/get_configs.ts
+++ lib/get_configs.ts
@@ -1,6 +1,6 @@
 import { axiosClient } from './axios_client'
 
 export const getConfig = async (key?: string) => {
-  const response = await axiosClient.get(`https://www.mgdesigns.pro/api/configs${key ? `?key=${key}` : ''}`)
+  const response = await axiosClient.get(`https://mgdesigns.pro/api/configs${key ? `?key=${key}` : ''}`)
   return response.data
 }
