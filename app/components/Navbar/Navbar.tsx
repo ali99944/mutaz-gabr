27f2a736636cffc2b39580f8265ae41b4938ad9f
ali99944 <alitarek99944@gmail.com>
2025-01-09 02:55:56 +0200

    9-1-2024 update

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -2,11 +2,11 @@ import { getConfig } from "@/lib/get_configs";
 import NavbarComponent from "./NavbarComponent";
 
 export default async function Page() {
-  const {
-    website_name
-  } = await getConfig('website_name')
+  // const {
+  //   website_name
+  // } = await getConfig('website_name')
 
   return <NavbarComponent 
-    website_name={website_name}
+    website_name={'Moataz gabr'}
   />
 }
